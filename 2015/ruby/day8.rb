d676f99df1740b79f58d848f0e084985d8c0ceb9
Rob Di Marco <rob.dimarco@416software.com>
2022-12-01 17:08:04 -0500

    Day 8

--- 2015/ruby/day8.rb
+++ 2015/ruby/day8.rb
@@ -4,15 +4,18 @@ end
 data = File.read('day8.txt')
 
 def char_count(str)
-  transformed_string = str
+  decoded_string = str
     .gsub(/\\x[a-f\d]{2}/, '.')
     .gsub(/\\\"/, '"')
     .gsub('\\\\', '\\')
-  # puts [str, transformed_string].join(' -> ')
-  [str.length, transformed_string.length - 2]
+  encoded_string = str
+    .gsub(/\\/, '\\\\\\')
+    .gsub('"', '\\"')
+  # puts [str, encoded_string].join(' -> ')
+  [str.length, decoded_string.length - 2, encoded_string.length + 2]
 end
 
-def answer(data)
+def answer_part_one(data)
   data = data.lines.map(&:strip)
   rv = data.reduce([0, 0]) do |acc, line|
     cnts = char_count(line)
@@ -24,11 +27,23 @@ def answer(data)
   rv[0] - rv[1]
 end
 
-assert_equal([2, 0], char_count("\"\""))
-assert_equal([5, 3], char_count("\"abc\""))
-assert_equal([10, 7], char_count("\"aaa\\\"aaa\""))
-assert_equal([6, 1], char_count("\"\\x27\""))
-assert_equal([27, 21], char_count("\"qludrkkvljljd\\\\xvdeum\\x4e\""))
+def answer_part_two(data)
+  data = data.lines.map(&:strip)
+  rv = data.reduce([0, 0]) do |acc, line|
+    cnts = char_count(line)
+    # puts cnts.inspect
+    [acc[0] + cnts[2], acc[1] + cnts[0]]
+  end
+  # puts rv
+
+  rv[0] - rv[1]
+end
+
+# assert_equal([2, 0, 6], char_count("\"\""))
+# assert_equal([5, 3, 9], char_count("\"abc\""))
+assert_equal([10, 7, 16], char_count("\"aaa\\\"aaa\""))
+assert_equal([6, 1, 11], char_count("\"\\x27\""))
+assert_equal([27, 21, 34], char_count("\"qludrkkvljljd\\\\xvdeum\\x4e\""))
 
 sample = <<~TXT
 ""
@@ -36,5 +51,6 @@ sample = <<~TXT
 "aaa\\"aaa"
 "\\x27"
 TXT
-assert_equal(12, answer(sample))
-puts "Part 1 #{answer(data)}"
+assert_equal(12, answer_part_one(sample))
+puts "Part 1 #{answer_part_one(data)}"
+puts "Part 2 #{answer_part_two(data)}"
