d246c0b8c73d30d4ff7d56370a6ca486ba7c5c61
Rob Di Marco <rob.dimarco@416software.com>
2022-12-01 19:45:25 -0500

    Day 12

--- 2015/ruby/day12.rb
+++ 2015/ruby/day12.rb
@@ -4,8 +4,22 @@ end
 
 data = File.read('day12.txt')
 
-def sum(inp)
-  inp.scan(/[\-\d]+/).map(&:to_i).sum
+def sum_json(json, skip_red = false)
+  return json if json.is_a?(Fixnum)
+
+  return json.map {|j| sum_json(j, skip_red)}.sum if json.is_a?(Array)
+
+  if json.is_a?(Hash)
+    return 0 if skip_red && json.values.any? {|v| v == 'red'}
+    return sum_json(json.keys, skip_red) + sum_json(json.values, skip_red)
+  end
+
+  0
+end
+require 'json'
+def sum(inp, skip_red = false)
+  # inp.scan(/[\-\d]+/).map(&:to_i).sum
+  sum_json(JSON.parse(inp), skip_red)
 end
 
 [
@@ -17,8 +31,12 @@ end
   [0, "[-1,{\"a\":1}]"],
   [0, "{}"],
   [0, "[]"],
+  # [4, "[1,{\"c\":\"red\",\"b\":2},3]"],
+  # [0, '{"d":"red","e":[1,2,3,4],"f":5}'],
+  # [6, '[1,"red",5]']
 ].each do |(exp, inp)|
   assert_equal(exp, sum(inp), "Input = #{inp}")
 end
 
 puts "Part 1 : #{sum(data)}"
+puts "Part 2 : #{sum(data, true)}"
