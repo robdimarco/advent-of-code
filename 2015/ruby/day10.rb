122d6afe1a9c0403bc6e08543ea8f11d4e404704
Rob Di Marco <rob.dimarco@416software.com>
2022-12-01 19:14:41 -0500

    Finish part 2

--- 2015/ruby/day10.rb
+++ 2015/ruby/day10.rb
@@ -18,11 +18,11 @@ def look_and_say(input)
       next
     end
 
-    rv += [cnt, last_char].join
+    rv << [cnt, last_char].join
     last_char = c
     cnt = 1
   end
-  rv += [cnt, last_char].join
+  rv << [cnt, last_char].join
 end
 
 [['11', '1'], ['21', '11'], ['1211', '21'], ['312211', '111221']].each do |(exp, inp)|
@@ -30,5 +30,5 @@ end
 end
 
 input = '1113222113'
-40.times { input = look_and_say(input)}
+50.times { |i| input = look_and_say(input); puts "Loop #{i + 1} at #{Time.now} length: #{input.size}"; }
 puts input.size
