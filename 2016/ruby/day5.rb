1d113f731e4a8100e01aab6915a01993ab879590
Rob Di Marco <rob.dimarco@416software.com>
2022-12-03 08:31:07 -0500

    Day 5 part 2

--- 2016/ruby/day5.rb
+++ 2016/ruby/day5.rb
@@ -4,7 +4,7 @@ end
 
 require 'digest'
 SEED = 'ojvtpuvg'
-def passcode(seed)
+def passcode_1(seed)
   rv = []
   i = 0
   while rv.size < 8 do
@@ -17,5 +17,26 @@ def passcode(seed)
   end
   rv.join
 end
-# assert_equal('18f47a30', passcode('abc'))
-puts "Part1 #{passcode(SEED)}"
+# assert_equal('18f47a30', passcode_1('abc'))
+# puts "Part1 #{passcode_1(SEED)}"
+
+
+def passcode_2(seed)
+  rv = {}
+  i = 0
+  while rv.size < 8 do
+    v = Digest::MD5.hexdigest [seed, i].join
+    c = v[5]
+    if v[0..4] == '00000'
+      if ('0'..'7').include?(c) && rv[c].nil?
+        rv[c] = v[6]
+        puts v
+        # print '.'
+      end
+    end
+    i += 1
+  end
+  ('0'..'7').map {|n| rv[n]}.join
+end
+# assert_equal('05ace8e3', passcode_2('abc'))
+puts "Part 2 #{passcode_2(SEED)}"
