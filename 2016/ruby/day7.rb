112b7842d116d65c93836f76234cb8d4eef97eb1
Rob Di Marco <rob.dimarco@416software.com>
2022-12-03 09:09:08 -0500

    Day 7 part 2

--- 2016/ruby/day7.rb
+++ 2016/ruby/day7.rb
@@ -3,7 +3,7 @@ def assert_equal(expected, actual, str="")
 end
 data = File.read('day7.txt')
 
-def valid?(str)
+def valid_tls?(str)
   checkable = true
   seq = ''
   chars = str.chars
@@ -21,13 +21,57 @@ def valid?(str)
   matched
 end
 
+def valid_ssl?(str)
+  supernet = []
+  hypernet = []
+
+  acc = ''
+  str.chars.each do |c|
+    if c == '['
+      supernet << acc
+      acc = ''
+    elsif c == ']'
+      hypernet << acc
+      acc = ''
+    else
+      acc << c
+    end
+  end
+  supernet << acc
+  babs = []
+  hypernet.each do |s|
+    (0..s.size - 2).each do |idx|
+      slice = s[idx..idx+2]
+      babs << slice if slice[0] == slice[2] && slice[0] != slice[1]
+    end
+  end
+  supernet.each do |s|
+    babs.each do |bab|
+      aba = [bab[1], bab[0], bab[1]].join
+      return true if s.include?(aba)
+    end
+  end
+
+  false
+end
+
 [
   [true, 'abba[mnop]qrst'],
   [false, 'abcd[bddb]xyyx'],
   [false, 'aaaa[qwer]tyui'],
   [true, 'ioxxoj[asdfgh]zxcvbn']
 ].each do |(exp, inp)|
-  assert_equal(exp, valid?(inp), inp)
+  assert_equal(exp, valid_tls?(inp), inp)
 end
-# puts data.lines.map {|l| [l, valid?(l.strip)].join(" ")}.join("\n")
-puts "Part 1: #{data.lines.select {|l| valid?(l.strip)}.size}"
+puts "Part 1: #{data.lines.select {|l| valid_tls?(l.strip)}.size}"
+
+[
+  [true, 'aba[bab]xyz'],
+  [false, 'xyx[xyx]xyx'],
+  [true, 'aaa[kek]eke'],
+  [true, 'zazbz[bzb]cdb']
+].each do |(exp, inp)|
+  assert_equal(exp, valid_ssl?(inp), inp)
+end
+puts "Part 2: #{data.lines.select {|l| valid_ssl?(l.strip)}.size}"
+
