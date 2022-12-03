3b22f0b39de82cd6e9634d0468b2ed17125e6e2e
Rob Di Marco <rob.dimarco@416software.com>
2022-12-02 21:20:34 -0500

    Day 23 part 1

---
+++ 2015/ruby/day23.rb
@@ -0,0 +1,67 @@
+def assert_equal(expected, actual, str="")
+  raise "Expected #{expected} but got #{actual}. #{str}" unless expected == actual
+end
+
+data = File.read('day23.txt')
+
+def execute(instructions, position, registers)
+  i = instructions[position]
+  cmd = i[0]
+  case cmd
+  when 'hlf'
+    registers[i[1]] /= 2
+  when 'tpl'
+    registers[i[1]] *= 3
+  when 'inc'
+    registers[i[1]] += 1
+  when 'jmp'
+    position += i[1].to_i
+    puts "#{cmd} at #{position} with #{registers}"
+    return [instructions, position, registers]
+  when 'jie'
+    if registers[i[1]] % 2 == 0
+      position += i[2].to_i
+      puts "#{cmd} at #{position} with #{registers}"
+      return [instructions, position, registers]
+    end    
+  when 'jio'
+    if registers[i[1]] == 1
+      position += i[2].to_i
+      puts "#{cmd} at #{position} with #{registers}"
+      return [instructions, position, registers]
+    end
+  else
+    raise "Unknown cmd #{cmd}"
+  end
+  puts "#{cmd} at #{position} with #{registers}"
+  position += 1
+
+  [instructions, position, registers]
+end
+
+def parse(data)
+  data.lines.map do |line| 
+    line.strip.split(/,?\s+/)
+  end
+end
+
+def run(data)
+  instructions = parse(data)
+  position = 0
+  registers = Hash.new(0)
+  while position < instructions.size do
+    instructions, position, registers = execute(instructions, position, registers)
+  end
+
+  registers
+end
+
+sample=<<~TEXT
+inc a
+jio a, +2
+tpl a
+inc a
+TEXT
+
+# assert_equal(2, run(sample)['a'])
+puts "Part 1: #{run(data)}"
