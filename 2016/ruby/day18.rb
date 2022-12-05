1f7413176fdde49685c8145fc590b4e7712286ff
Rob Di Marco <rob.dimarco@416software.com>
2022-12-04 23:49:52 -0500

    Day 18 part 2

--- 2016/ruby/day18.rb
+++ 2016/ruby/day18.rb
@@ -8,6 +8,7 @@ TRAP_GENS = ['^^.', '.^^', '^..', '..^']
 def safe_tiles(row_string, total)
   row = row_string.chars
   rows = [row]
+  # row_hash = {}
   while rows.size < total do
     lrow = rows[-1]
     nrow = (0...lrow.size).map do |n|
@@ -18,12 +19,25 @@ def safe_tiles(row_string, total)
       TRAP_GENS.include?([left, center, right].join) ? '^' : '.'
     end
 
+    puts "Row count #{rows.size}" if rand(1000) == 0
+
+    # if row_hash.key?(nrow)
+    #   puts "Got match"
+    #   for_pattern = rows.flatten.join.chars.count {|c| c == '.'}
+    #   extra_lines = total % for_pattern.size
+
+    #   extra_vals = rows[0...extra_lines].flatten.join.chars.count {|c| c == '.'}
+
+    #   return for_pattern * total / for_pattern.size + extra_vals
+    # end
+
     rows.push(nrow)
   end
-  rows.flatten.join.chars.count {|c| c == '.'}
+  rows.map {|row| row.count {|c| c == '.'}}.sum
 end
 
 assert_equal(6, safe_tiles('..^^.', 3))
 assert_equal(38, safe_tiles('.^^.^.^^^^', 10))
 
 puts "Part 1: #{safe_tiles(data, 40)}"
+puts "Part 2: #{safe_tiles(data, 400000)}"
