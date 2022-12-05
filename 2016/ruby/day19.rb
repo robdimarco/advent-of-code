2b83a651c2cfc252cec1de145669e78c7eafd975
Rob Di Marco <rob.dimarco@416software.com>
2022-12-05 10:53:41 -0500

    Day 19 part 2

--- 2016/ruby/day19.rb
+++ 2016/ruby/day19.rb
@@ -22,5 +22,28 @@ def winner(positions)
   end
   ar[0]
 end
+
 assert_equal(3, winner(5))
-puts "Part 1: #{winner(input)}"
+# puts "Part 1: #{winner(input  )}"
+require 'algorithms'
+def winner_part2(positions)
+  cut = (positions + 1) / 2
+  left = Containers::CDeque.new((1..cut).to_a)
+  right = Containers::CDeque.new(((cut+1)..positions).to_a.reverse)
+ 
+  while (left.size + right.size) > 1
+    if left.size > right.size
+      left.pop_back
+    else
+      right.pop_back
+    end
+    right.push_front(left.pop_front)
+    left.push_back(right.pop_back)
+    puts (left.size + right.size) if rand(1000) == 0
+  end
+
+  [left.pop_front, right.pop_front].compact[0]
+end
+
+assert_equal(2, winner_part2(5))
+puts "Part 2: #{winner_part2(input)}"
