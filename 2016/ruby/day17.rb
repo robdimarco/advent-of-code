a76ccb6c52a17b6d85bfdbcf77b0131ba27154f3
Rob Di Marco <rob.dimarco@416software.com>
2022-12-04 23:19:10 -0500

    Finish day 17

--- 2016/ruby/day17.rb
+++ 2016/ruby/day17.rb
@@ -5,7 +5,7 @@ require 'digest'
 DIRS = [['U', [-1, 0]], ['D', [1, 0]], ['L', [0, -1]], ['R', [0, 1]]]
 ENDPOINT = [3, 3]
 
-def shortest_path(code, route: "", pos: [0, 0], path: [])
+def shortest_path(code, route: "", pos: [0, 0], path: [], longest_path: false)
   # puts "At #{pos.inspect} from #{route}"
   return route if pos == ENDPOINT
 
@@ -16,19 +16,42 @@ def shortest_path(code, route: "", pos: [0, 0], path: [])
   return nil if open_doors.empty?
 
   new_path = [pos] + path
-  routes = open_doors.map do |(label, delta)|
+  possible_routes = open_doors.map do |(label, delta)|
     dx, dy = delta
     new_pos = [dx + pos[0], dy + pos[1]]
     next if new_pos[0] < 0 || new_pos[0] > ENDPOINT[0] || new_pos[1] < 0 || new_pos[1] > ENDPOINT[1]
-    # next if path.include?(new_pos)
     
-    shortest_path(code, route: route + label, pos: new_pos, path: new_path)
+    [label, new_pos]
   end.compact
-  routes.min_by(&:size)
+
+  if longest_path
+    possible_routes.reject! do |(label, new_pos)|
+      possible_routes.size > 1 && new_pos == ENDPOINT
+    end
+  end
+
+  routes = possible_routes.map do |(label, new_pos)|
+    shortest_path(code, route: route + label, pos: new_pos, path: new_path, longest_path: longest_path)
+  end.compact
+
+  # puts routes.inspect if longest_path
+  # require 'debug'
+  # binding.break if longest_path && path.empty?
+  longest_path ? routes.max_by(&:size) : routes.min_by(&:size)
 end
 
-assert_equal("DDRRRD", shortest_path("ihgpwlah"))
-assert_equal("DDUDRLRRUDRD", shortest_path("kglvqrro"))
-assert_equal("DRURDRUDDLLDLUURRDULRLDUUDDDRR", shortest_path("ulqzkmiv"))
+# assert_equal("DDRRRD", shortest_path("ihgpwlah"))
+# assert_equal("DDUDRLRRUDRD", shortest_path("kglvqrro"))
+# assert_equal("DRURDRUDDLLDLUURRDULRLDUUDDDRR", shortest_path("ulqzkmiv"))
 
-puts "Part 1: #{shortest_path("dmypynyp")}"
+
+# puts "Part 1: #{shortest_path("dmypynyp")}"
+
+def longest_path(code)
+  shortest_path(code, longest_path: true)
+end
+
+assert_equal(492, longest_path('kglvqrro').size)
+assert_equal(830, longest_path('ulqzkmiv').size)
+puts "Part 2: #{longest_path("dmypynyp").size}"
+# assert_equal(370, longest_path('ihgpwlah').size)
