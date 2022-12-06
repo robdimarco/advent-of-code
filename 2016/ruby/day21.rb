07b13ef5a7030ae8dc62abeee4303f19d0622632
Rob Di Marco <rob.dimarco@416software.com>
2022-12-05 19:14:14 -0500

    Day 21 part 2

--- 2016/ruby/day21.rb
+++ 2016/ruby/day21.rb
@@ -21,6 +21,7 @@ def swap_position(str, a, b)
 end
 
 assert_equal('ebcda', swap_position('abcde', 4, 0))
+assert_equal('abcde', swap_position('ebcda', 0, 4))
 
 def swap_letter(str, a, b)
   str.chars.map do |c|
@@ -34,11 +35,13 @@ def swap_letter(str, a, b)
   end.join
 end
 assert_equal('edcba', swap_letter('ebcda', 'b', 'd'))
+assert_equal('ebcda', swap_letter('edcba', 'b', 'd'))
 
 def reverse(str, start, to)
   str[0...start] + str[start..to].reverse + str[(to + 1)..-1]
 end
 assert_equal('abcde', reverse('edcba', 0, 4))
+assert_equal('edcba', reverse('abcde', 0, 4))
 
 def rotate(str, dir, steps)
   inc = dir == :left ? 1 : -1
@@ -48,17 +51,32 @@ def rotate(str, dir, steps)
   end.join
 end
 assert_equal('bcdea', rotate('abcde', :left, 1))
+assert_equal('abcde', rotate('bcdea', :right, 1))
 assert_equal('eabcd', rotate('abcde', :right, 1))
+assert_equal('abcde', rotate('eabcd', :left, 1))
 assert_equal('abcde', rotate('abcde', :right, 5))
 assert_equal('eabcd', rotate('abcde', :right, 11))
 
-def rotate_based_on_position(str, char)
-  steps = str.index(char)
-  steps += 1 if steps >= 4
-  steps += 1
-  rotate(str, :right, steps)
+def rotate_based_on_position(str, char, dir=:right)
+  # require 'debug'; binding.break
+  if dir == :right
+    steps = str.index(char)
+    steps += 1 if dir == :right && steps >= 4
+    steps += 1
+    rotate(str, dir, steps)
+  else
+    rv = nil
+    i = 1
+    while rv.nil?
+      t = rotate(str, :left, i)
+      rv = t if rotate_based_on_position(t, char) == str
+      i += 1
+    end
+    rv
+  end
 end
 assert_equal('ecabd', rotate_based_on_position('abdec', 'b'))
+assert_equal('abdec', rotate_based_on_position('ecabd', 'b', :left))
 assert_equal('decab', rotate_based_on_position('ecabd', 'd'))
 
 def move(str, from, to)
@@ -100,6 +118,29 @@ def scrambled(password, instructions)
   end
   password
 end
-
 assert_equal('decab', scrambled('abcde', SAMPLE))
 puts "Part 1: #{scrambled('abcdefgh', DATA)}"
+
+def unscrambled(password, instructions)
+  instructions = instructions.lines.map {|l| parse_line(l)}
+  instructions.reverse.each do |i|
+    cmd = i[0]
+    args = i[1..-1]
+    case cmd
+    when :swap_position, :swap_letter, :reverse
+      # no op
+    when :rotate_based_on_position
+      args << :left
+    when :rotate
+      args[0] = args[0] == :left ? :right : :left
+    when :move
+      args = args.reverse
+    end
+    password = send(cmd, password, *args)
+  end
+  password  
+end
+
+assert_equal('abcde', unscrambled('decab', SAMPLE))
+assert_equal('abcdefgh', unscrambled(scrambled('abcdefgh', DATA), DATA))
+puts "Part2: #{unscrambled('fbgdceah', DATA)}"
