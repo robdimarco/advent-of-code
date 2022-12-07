1d9c0d9591747f7980bd7d2a5e736d0aea6d11a2
Rob Di Marco <rob.dimarco@416software.com>
2022-12-07 01:36:09 -0500

    Day 4

--- 2017/ruby/day4.rb
+++ 2017/ruby/day4.rb
@@ -3,12 +3,21 @@ def assert_equal(expected, actual, str="")
 end
 DATA = File.read('day4.txt')
 
-def valid?(str)
+def valid_unique?(str)
   tokens = str.split(' ')
   tokens.size == tokens.uniq.size
 end
-assert_equal(true, valid?('aa bb cc dd ee'))
-assert_equal(false, valid?('aa bb cc dd aa'))
-assert_equal(true, valid?('aa bb cc dd aaa'))
+assert_equal(true, valid_unique?('aa bb cc dd ee'))
+assert_equal(false, valid_unique?('aa bb cc dd aa'))
+assert_equal(true, valid_unique?('aa bb cc dd aaa'))
+
+puts "Part 1# #{DATA.lines.count {|l| valid_unique?(l.strip)}}"
+
+def valid?(str)
+  tokens = str.split(' ').map {|s| s.chars.sort}
+  tokens.size == tokens.uniq.size
+end
+assert_equal(true, valid?('abcde fghij'))
+assert_equal(false, valid?('abcde xyz ecdab'))
 
 puts "Part 1# #{DATA.lines.count {|l| valid?(l.strip)}}"
