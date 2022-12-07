0d675ea9d2d237b0808d19282b743cc2f04bded8
Rob Di Marco <rob.dimarco@416software.com>
2022-12-07 00:32:55 -0500

    Day 7 part 1

---
+++ 2022/ruby/day7.rb
@@ -0,0 +1,95 @@
+def assert_equal(expected, actual, str="")
+  raise "Expected #{expected} but got #{actual}. #{str}" unless expected == actual
+end
+DATA = File.read("day7.txt")
+SAMPLE = <<~TEXT
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+TEXT
+AOCFile = Struct.new(:size, :name)
+AOCDir = Struct.new(:name, :contents) do 
+  def size
+    contents.sum(&:size)
+  end
+
+  def files
+    contents.select {|d| d.is_a?(AOCFile)}
+  end
+
+  def dirs
+    contents.select {|d| d.is_a?(AOCDir)}
+  end
+
+  def find(name)
+    contents.find {|i| i.name == name}
+  end
+
+  def all_dirs
+    dirs.reduce([self]) do |acc, d|
+      acc += d.all_dirs
+      acc
+    end
+  end
+end
+
+def parse_input(input)
+  root = AOCDir.new('/', [])
+  dir_history = []
+
+  input.lines.each do |l|
+    # require 'debug'; binding.break
+    if l.start_with?('$')
+      _, cmd, args = l.split(/\s+/)
+      if cmd == 'cd'
+        case args
+        when '/'
+          dir_history.push(root)
+        when '..'
+          dir_history = dir_history[0..-2]
+        else
+          dir = dir_history[-1].find(args)
+          dir_history.push(dir) if dir
+        end
+      end
+    else
+      tokens = l.split(/\s+/)
+      dir = dir_history[-1]
+      if tokens[0] == 'dir'
+        dir.contents.push(AOCDir.new(tokens[1], []))
+      else
+        dir.contents.push(AOCFile.new(tokens[0].to_i, tokens[1]))
+      end
+    end
+  end
+  root
+end
+
+def file_size(input)
+  dir = parse_input(input)
+  pp dir
+  dir.all_dirs.select {|d| d.size < 100_000}.sum(&:size)
+end
+
+assert_equal(95437, file_size(SAMPLE))
+puts "Part 1 #{file_size(DATA)}"
