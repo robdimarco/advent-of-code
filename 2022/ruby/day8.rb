ad9bbbc9d4c6ef4bf6573b066c29f6c5ff0da642
Rob Di Marco <rob.dimarco@416software.com>
2022-12-08 00:57:19 -0500

    Day 8

--- 2022/ruby/day8.rb
+++ 2022/ruby/day8.rb
@@ -10,7 +10,7 @@ SAMPLE = <<~TEXT
 35390
 TEXT
 
-def run(input)
+def parse(input)
   trees = {}
   grid = input.lines.each_with_index do |l, y|
     l.strip.chars.each_with_index do |c, x|
@@ -36,6 +36,11 @@ def run(input)
       end
     end
   end
+  [trees, grid]
+end
+
+def run(input)
+  trees, _ = parse(input)
 
   trees.values.count {|t| t[:visible]}
 end
@@ -44,6 +49,36 @@ assert_equal(21, run(SAMPLE))
 puts "Part 1 #{run(DATA)}"
 
 def scenic_score(data)
+  trees, grid = parse(data)
+  (0...grid.size).each do |x|
+    (0...grid.size).each do |y|
+      tree = trees[[x, y]]
+      cnts = [[0,1], [1, 0], [-1, 0], [0, -1]].map do |(dx, dy)|
+        pos = [x, y]
+        cnt = 0
+        loop do
+          pos = [pos[0] + dx, pos[1] + dy]
+          t_p = trees[pos]
+          if t_p.nil?
+            break
+          elsif t_p[:height] < tree[:height]
+            cnt += 1
+          elsif t_p[:height] == tree[:height]
+            cnt += 1
+            break
+          else
+            cnt +=1 if dx == 1 || dy == 1
+            break
+          end
+        end
+        cnt
+      end
+      # pp cnts
+      tree[:scenic] = cnts.reduce(1, &:*)
+    end
+  end
+  # pp trees
+  trees.values.map {|n| n[:scenic]}.max
 end
 assert_equal(8, scenic_score(SAMPLE))
 puts "Part 2 #{scenic_score(DATA)}"
