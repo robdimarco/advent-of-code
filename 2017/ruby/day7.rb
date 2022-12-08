2b7e076c6687dce5d7fa0d64f46b29db76ee0db0
Rob Di Marco <rob.dimarco@416software.com>
2022-12-07 20:41:44 -0500

    Day 7

--- 2017/ruby/day7.rb
+++ 2017/ruby/day7.rb
@@ -23,10 +23,38 @@ def bottom(data)
     tokens = line.strip.split(/\s+/)
     {name: tokens[0], weight: tokens[1].scan(/\d+/).first.to_i, subs: Array(tokens[3..-1]).map {|n| n.gsub(',', '')}}
   end
-  # puts programs.flat_map {|n| n[:subs]}
+  # puts programs.flat_map {|n| n[:weight]}.inspect
 
   (programs.map {|n|n[:name]} - programs.flat_map {|n| n[:subs]}).first
 end
 
+def total_weight(name, programs)
+  target = programs[name]
+  return target[:total_weight] if target[:total_weight]
+
+  wts = target[:subs].map {|n| total_weight(n, programs) }
+  target[:sub_wts] = Array(wts)
+  # puts "#{name} = #{wts.sum} + #{target[:weight]}"
+  target[:total_weight] = wts.sum + target[:weight]
+end
+
+def missing_weight(data)
+  programs = data.strip.lines.each_with_object({}) do |line, hsh|
+    tokens = line.strip.split(/\s+/)
+    hsh[tokens[0]] = {name: tokens[0], weight: tokens[1].scan(/\d+/).first.to_i, subs: Array(tokens[3..-1]).map {|n| n.gsub(',', '')}}
+  end
+  programs.each {|name, program| total_weight(name, programs)}
+  bad = programs.values.sort_by {|p| p[:total_weight]}.find {|p| p[:sub_wts].uniq.size > 1}
+  single, multi = bad[:sub_wts].tally.partition {|_,v| v == 1}
+  bad_wt = single[0][0]
+  gd_wt = multi[0][0]
+
+  bad_p = bad[:subs].map {|n| programs[n]}.find {|p| p[:total_weight] == bad_wt}
+  bad_p[:weight] - bad_wt + gd_wt
+end
+
 assert_equal('tknk', bottom(SAMPLE))
 puts "Part 1: #{bottom(DATA)}"
+
+assert_equal(60, missing_weight(SAMPLE))
+puts "Part 2: #{missing_weight(DATA)}"
