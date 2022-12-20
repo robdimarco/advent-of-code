818633c1a8c7aaa949461bdf7ce99a3f014a7f62
Rob Di Marco <rob.dimarco@416software.com>
2022-12-20 08:21:25 -0500

    Day 20 part 2

--- 2022/ruby/day20.rb
+++ 2022/ruby/day20.rb
@@ -17,18 +17,20 @@ SAMPLE = <<~TEXT
 4
 TEXT
 
-def part1(input, get = [1_000,2_000,3_000])
+def part1(input, get: [1_000,2_000,3_000], key: 1, mixes: 1)
   start = []
-  input.lines.each_with_index {|n, idx| start.push([n.to_i, idx])}
+  input.lines.each_with_index {|n, idx| start.push([n.to_i * key, idx])}
   current = start.dup
-  start.each do |entry|
-    # binding.break
-    n, _ = entry
-    idx = current.index(entry)
-    current.delete(entry)
-    current.rotate!(n)
-    current.insert(idx, entry)
-    # puts current.map(&:first).inspect
+  mixes.times do
+    start.each do |entry|
+      # binding.break
+      n, _ = entry
+      idx = current.index(entry)
+      current.delete(entry)
+      current.rotate!(n)
+      current.insert(idx, entry)
+      # puts current.map(&:first).inspect
+    end
   end
   # binding.break
   vals = current.map(&:first)
@@ -39,9 +41,12 @@ def part1(input, get = [1_000,2_000,3_000])
   end
 end
 
-def part2(input)
+def part2(input, get: [1_000,2_000,3_000], key: 811589153, mixes: 10)
+  part1(input, get: get, key: key, mixes: mixes)
 end
 
 assert_equal(3, part1(SAMPLE))
 
 puts "Part 1: #{part1(DATA)}"
+assert_equal(1623178306, part2(SAMPLE))
+puts "Part 2: #{part2(DATA)}"
