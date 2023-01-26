f97561256980be12507546e2a69bff6f01d7433d
Rob Di Marco <rob.dimarco@416software.com>
2023-01-26 01:08:33 -0500

    Day 6 part 1

---
+++ 2015/go/day6/day6.go
@@ -0,0 +1,59 @@
+package day6
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Instruction struct {
+	Cmd      string
+	StartPos [2]int
+	EndPos   [2]int
+}
+
+func GetInstructions(input []string) []Instruction {
+	rv := make([]Instruction, len(input))
+	for idx, str := range input {
+		rv[idx] = GetInstruction(str)
+	}
+	return rv
+}
+
+func GetInstruction(input string) Instruction {
+	var cmd string
+	var row1, row2, col1, col2 int
+	if strings.HasPrefix(input, "turn") {
+		fmt.Sscanf(input, "turn %s %d,%d through %d,%d", &cmd, &row1, &col1, &row2, &col2)
+	} else {
+		fmt.Sscanf(input, "%s %d,%d through %d,%d", &cmd, &row1, &col1, &row2, &col2)
+	}
+	return Instruction{Cmd: cmd, StartPos: [2]int{row1, col1}, EndPos: [2]int{row2, col2}}
+}
+
+func Part1(input []string) int {
+	grid := make([][]int, 1000)
+	for i := 0; i < 1000; i++ {
+		grid[i] = make([]int, 1000)
+	}
+	for _, instruction := range GetInstructions(input) {
+		for row := instruction.StartPos[0]; row <= instruction.EndPos[0]; row++ {
+			for col := instruction.StartPos[1]; col <= instruction.EndPos[1]; col++ {
+				switch instruction.Cmd {
+				case "on":
+					grid[row][col] = 1
+				case "off":
+					grid[row][col] = 0
+				case "toggle":
+					grid[row][col] = (grid[row][col] * -1) + 1
+				}
+			}
+		}
+	}
+	sum := 0
+	for _, row := range grid {
+		for _, val := range row {
+			sum += val
+		}
+	}
+	return sum
+}
---
+++ 2015/go/day6/day6_test.go
@@ -0,0 +1,85 @@
+package day6
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/robdimarco/AdventOfCode/2015/utils"
+)
+
+func TestGetInstruction(t *testing.T) {
+	cases := []struct {
+		given    string
+		expected Instruction
+	}{{
+		given:    "turn on 489,959 through 759,964",
+		expected: Instruction{Cmd: "on", StartPos: [2]int{489, 959}, EndPos: [2]int{759, 964}},
+	}, {
+		given:    "turn off 820,516 through 871,914",
+		expected: Instruction{Cmd: "off", StartPos: [2]int{820, 516}, EndPos: [2]int{871, 914}},
+	}, {
+		given:    "toggle 0,0 through 999,0",
+		expected: Instruction{Cmd: "toggle", StartPos: [2]int{0, 0}, EndPos: [2]int{999, 0}},
+	}}
+
+	for _, tt := range cases {
+		t.Run("Test", func(t *testing.T) {
+			result := GetInstruction(tt.given)
+			if result != tt.expected {
+				t.Errorf("expected %#v, but got %#v", tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestGetInstructions(t *testing.T) {
+	cases := []struct {
+		given    []string
+		expected []Instruction
+	}{{
+		given:    []string{"turn on 489,959 through 759,964", "turn off 820,516 through 871,914"},
+		expected: []Instruction{{Cmd: "on", StartPos: [2]int{489, 959}, EndPos: [2]int{759, 964}}, {Cmd: "off", StartPos: [2]int{820, 516}, EndPos: [2]int{871, 914}}},
+	}}
+
+	for _, tt := range cases {
+		t.Run("Test", func(t *testing.T) {
+			got := GetInstructions(tt.given)
+			if diff := cmp.Diff(tt.expected, got); diff != "" {
+				t.Errorf("GetInstructions() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	var want, got int
+	want = 0
+	got = Part1([]string{})
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part1() mismatch (-want +got):\n%s", diff)
+	}
+
+	want = 4
+	got = Part1([]string{"turn on 499,499 through 500,500"})
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part1() mismatch (-want +got):\n%s", diff)
+	}
+
+	want = 1_000_000
+	got = Part1([]string{"turn on 0,0 through 999,999"})
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part1() mismatch (-want +got):\n%s", diff)
+	}
+
+	want = 999_996
+	got = Part1([]string{"turn on 0,0 through 999,999", "turn off 499,499 through 500,500"})
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part1() mismatch (-want +got):\n%s", diff)
+	}
+
+	want = 569999
+	got = Part1(utils.InputFileAsLines("day6"))
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part1() mismatch (-want +got):\n%s", diff)
+	}
+}
