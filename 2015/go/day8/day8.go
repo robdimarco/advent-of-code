5bef5fc98ac9c50c6b75e18c42f43febe0dea5a8
Rob Di Marco <rob.dimarco@416software.com>
2023-01-26 17:16:11 -0500

    2015 go Day 8 Part 2

--- 2015/go/day8/day8.go
+++ 2015/go/day8/day8.go
@@ -8,6 +8,30 @@ func Part1(lines []string) int {
 	}
 	return Cnt - CharCnt
 }
+func Part2(lines []string) int {
+	var Cnt, CharCnt int
+	for _, line := range lines {
+		Cnt += len(line)
+		CharCnt += EncodeStringLen(line)
+	}
+	return CharCnt - Cnt
+}
+
+func EncodeStringLen(s string) int {
+	rv := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		switch c {
+		case '"':
+			rv = append(rv, '\\')
+		case '\\':
+			rv = append(rv, '\\')
+		}
+
+		rv = append(rv, c)
+	}
+	return len(rv) + 2
+}
 
 func EffectiveCharCount(s string) int {
 	pos := 0
--- 2015/go/day8/day8_test.go
+++ 2015/go/day8/day8_test.go
@@ -22,6 +22,21 @@ func TestPart1Actual(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	want := 19
+	got := Part2([]string{"\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\""})
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part2() mismatch (-want +got):\n%s", diff)
+	}
+}
+func TestPart2Actual(t *testing.T) {
+	want := 2117
+	got := Part2(utils.InputFileAsLines("day8"))
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Part2() mismatch (-want +got):\n%s", diff)
+	}
+}
+
 func TestEffectiveCharCount(t *testing.T) {
 	cases := []struct {
 		given    string
@@ -46,6 +61,35 @@ func TestEffectiveCharCount(t *testing.T) {
 			got := EffectiveCharCount(tt.given)
 			if diff := cmp.Diff(want, got); diff != "" {
 				t.Errorf("EffectiveCharCount() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestEncodeStringLen(t *testing.T) {
+	cases := []struct {
+		given    string
+		expected int
+	}{{
+		given:    "\"\"",
+		expected: 6,
+	}, {
+		given:    "\"abc\"",
+		expected: 9,
+	}, {
+		given:    "\"aaa\\\"aaa\"",
+		expected: 16,
+	}, {
+		given:    "\"\\x27\"",
+		expected: 11,
+	}}
+
+	for _, tt := range cases {
+		t.Run("TestEncodeStringLen", func(t *testing.T) {
+			want := tt.expected
+			got := EncodeStringLen(tt.given)
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("EncodeStringLen() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
