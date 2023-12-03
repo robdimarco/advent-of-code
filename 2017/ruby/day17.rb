fbbe5379911cb58e5b9bb0130662b652e59d566b
Rob Di Marco <rob.dimarco@416software.com>
2023-12-03 14:47:41 -0500

    Day 17 part 2

--- 2017/ruby/day17.rb
+++ 2017/ruby/day17.rb
@@ -1,4 +1,4 @@
-def process(step, iters)
+def part_1(step, iters)
   a = [0]
   pos = 0
   iters.times do |i|
@@ -12,4 +12,21 @@ def process(step, iters)
   puts a[pos + 1]
 end
 
-process(304, 2017)
+part_1(304, 2017)
+
+def part_2(step, iters)
+  pos = 0
+  curr_after = 0
+  iters.times do |i|
+    n = i + 1
+    pos = (pos + step) % (i + 1)
+    # puts "inserting at #{pos}"
+    # a = a[0..pos] + [n] + a[pos+1..-1]
+    curr_after = n if pos == 0
+    # puts "updating to #{curr_after}"
+    pos += 1
+    # puts a.join(' ')
+  end
+  puts curr_after
+end
+part_2(304, 50_000_000)
