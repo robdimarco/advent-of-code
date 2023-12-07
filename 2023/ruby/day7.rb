2ad30e141546ab18cdedff703d42bea2ef3f8ccf
Rob Di Marco <rob.dimarco@416software.com>
2023-12-07 00:48:43 -0500

    Day 7 part 2

--- 2023/ruby/day7.rb
+++ 2023/ruby/day7.rb
@@ -9,6 +9,7 @@ TXT
 real = File.open("day7.txt").read.lines.map(&:strip)
 
 RANKS = %w(A K Q J T 9 8 7 6 5 4 3 2)
+RANKS_2 = %w(A K Q T 9 8 7 6 5 4 3 2 J)
 
 def score(hand)
   h = hand.chars.each_with_object(Hash.new(0)) do |c, h|
@@ -28,6 +29,28 @@ def score(hand)
     7
   end
 end
+def score_2(hand)
+  h = hand.chars.each_with_object(Hash.new(0)) do |c, h|
+    h[c] += 1
+  end
+
+  mv_x = h.reject{|k,v| k == 'J'}.values.max || 5
+
+  mv_xj = mv_x + h['J']
+  
+  case mv_xj
+  when 5, 10
+    1
+  when 4
+    2
+  when 3
+    h.count {|k,v| k != 'J' && v >= 2} == 2 ? 3 : 4
+  when 2
+    h.size == 3 ? 5 : 6
+  else
+    7
+  end
+end
 
 def part1(lines)
   data = lines.map {|l| a, b = l.split(' '); [a, b.to_i]}
@@ -35,8 +58,6 @@ def part1(lines)
     s = score(card)
     [s] + card.chars.map {|c| RANKS.index(c)}
   end
-  rv
-  puts rv.inspect
   sum = 0
 
   rv.reverse.each_with_index do |o, i|
@@ -45,11 +66,23 @@ def part1(lines)
   sum
 end
 
-def part2(data)
+def part2(lines)
+  data = lines.map {|l| a, b = l.split(' '); [a, b.to_i]}
+  rv = data.sort_by do |(card, value)|
+    s = score_2(card)
+    [s] + card.chars.map {|c| RANKS_2.index(c)}
+  end
+
+  sum = 0
+
+  rv.reverse.each_with_index do |o, i|
+    sum += o.last * (i+1)
+  end
+  sum
 end
 
-puts part1(sample)
-puts part1(real)
+# puts part1(sample)
+# puts part1(real)
 
-# puts part2(sample)
-# puts part2(real)
+puts part2(sample)
+puts part2(real)
