915f9e2b85300cae7ff6d6756c292848d953f6b4
Rob Di Marco <rob.dimarco@416software.com>
2023-12-08 00:26:19 -0500

    Day 8 part 2

--- 2023/ruby/day8.rb
+++ 2023/ruby/day8.rb
@@ -18,6 +18,19 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 TXT
 
+sample3 =<<~TXT.lines.map(&:strip)
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+TXT
+
 real = File.open("day8.txt").read.lines.map(&:strip)
 
 def parse(lines)
@@ -25,7 +38,7 @@ def parse(lines)
   lines.shift
   elements = lines.each_with_object({}) do |l, h|
     start, opts = l.split(' = ')
-    a,b = opts.scan(/[A-Z]+/)
+    a,b = opts.scan(/[A-Z\d]+/)
     h[start] = {L: a, R: b}
   end
 
@@ -45,17 +58,65 @@ def journey(directions, elements)
   i
 end
 
+def journey2(directions, elements)
+  all_pos = elements.keys.select {|s| s[-1] == 'A'}
+  dists = all_pos.map do |pos|
+    i = 0
+    loop do
+      dir = directions[i % directions.size]
+      cur = elements[pos]
+      pos = cur[dir.to_sym]
+      i += 1
+      break if pos[-1] == 'Z'
+    end
+    i
+  end
+  reduce(dists)
+end
+
+def reduce(nums)
+  rv = Hash.new(0)
+  nums.each do |n|
+    f = factors(n)
+    f.each do |ff, c|
+      rv[ff] = c if rv[ff] < c
+      # puts rv.inspect
+    end
+  end
+  rv.map {|k,v| k**v}.reduce(:*)
+end
+
+def factors(num)
+  factors = []
+  i = 2
+  loop do
+    if num % i == 0
+      num = num / i
+      factors.push(i)
+    else
+      i += 1
+    end
+
+    break if num == 1
+  end
+  factors.each_with_object(Hash.new(0)) {|n, h| h[n] += 1}
+end
+
 def part1(lines)
   a, b = parse(lines)
   journey(a, b)
 end
 
 def part2(lines)
+  a, b = parse(lines)
+  journey2(a, b)
 end
 
-puts part1(sample)
-puts part1(sample2)
-puts part1(real)
+# puts part1(sample)
+# puts part1(sample2)
+# puts part1(real)
 
-# puts part2(sample)
-# puts part2(real)
+puts part2(sample3)
+puts part2(real)
+# puts factors(300)
+# puts reduce([10, 300, 7, 14])
