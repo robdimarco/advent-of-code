b344fe93109a6597905b11f939773e121d0ade0c
Rob Di Marco <rob.dimarco@416software.com>
2023-12-12 08:20:20 -0500

    Day 12...part 2...not finishing

--- 2023/ruby/day12.rb
+++ 2023/ruby/day12.rb
@@ -9,46 +9,110 @@ TXT
 
 real = File.open("day12.txt").read.lines.map(&:strip)
 
-def count_for_line(line)
-  line.chars.count {|c| c == '?'} 
+def strip(pattern)
+  2.times do
+    loop do 
+      break if pattern.empty? || pattern[0] != '.'
+      pattern = pattern[1..-1]
+    end
+    pattern.reverse
+  end
+  pattern
 end
 
-def combos_for_line(line)
-  a = %w(# .)
-  cnt = count_for_line(line)
-  rv = []
-  (2**cnt).times do |i|
-    idxs = i.to_s(2).rjust(cnt, '0').chars.map(&:to_i)
-    l = line
-    idxs.each do |idx|
-      l = l.sub('?', a[idx])
+def counts(pattern)
+  rv = [0]
+  pattern.each do |c|
+    if c =='#'
+      rv[-1] += 1 
+    else
+      rv.push(0) if rv[-1] > 0
     end
-    rv << l
   end
+  rv = rv[0..-2] if rv[-1] == 0
   rv
+end
+
+def valid?(pattern, arrangement)
+  cnt = 0
+  arrangement = arrangement.dup
+  a = arrangement.shift.to_i
+  pattern.each do |c|
+    case c
+    when '#'
+      cnt += 1
+      return false if cnt > a
+    when '.'
+      if cnt > 0
+        return false if cnt < a
+        a = arrangement.shift.to_i
+        cnt = 0
+      end
+    when '?'
+      return true
+    end
+  end
+  cnt == a
+end
+# puts valid?('????'.chars, [3])
+# puts valid?('.???'.chars, [3])
+# puts valid?('.#.?????'.chars, [3])
+# puts valid?('.#'.chars, [3])
+# puts valid?('.###'.chars, [3])
+# puts valid?('.###.'.chars, [3])
+# puts valid?('.##?.'.chars, [3])
+# puts valid?('.##.?.'.chars, [3])
+# puts valid?('##.?.'.chars, [3])
+# puts valid?('###.?.'.chars, [3])
+
+def combos_for_line(args)
+  cnt = 0
+  to_check = [args]
+  while to_check.any?
+    pattern, arrangement = to_check.shift
+    pattern=strip(pattern)
+
+    # If no more substitutions can be made, we can look at the count match
+    next_sub = pattern.index('?')
+    if next_sub.nil?
+      cnt += 1 if counts(pattern) == arrangement
+      next
+    end
+
+    ['#', '.'].each do |c|
+      npattern = pattern.dup
+      npattern[next_sub] = c
+      to_check.push([npattern, arrangement]) if valid?(npattern, arrangement)
+    end
+  end
+  cnt
 end
 
 def parse(lines)
   lines.map do |line|
     a, b = line.split(' ')
-    {pattern: a, arrangements: b.split(',').map(&:to_i)}
+    [a.chars, b.split(',').map(&:to_i)]
   end
 end
 
 def part1(lines)
   data = parse(lines)
   data.map do |d|
-    c = combos_for_line(d[:pattern])
-    c.select {|cc| cc.scan(/\#+/).map(&:size) == d[:arrangements]}.size
+    combos_for_line(d)
   end.sum
 end
 
 def part2(lines)
+  data = parse(lines)
+  data.map do |(a, b)|
+    a = ([a] * 5).map(&:join).join("?").chars
+    combos_for_line([a, b *5])
+  end.sum
 end
 
 puts part1(sample)
-puts part1(real)
+# puts part1(real)
 
-# puts part2(sample)
+puts part2(sample)
 # puts part2(real)
 
