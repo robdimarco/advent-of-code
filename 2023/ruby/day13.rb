b923d7ffa9ca6563325b4dba32b114517352532b
Rob Di Marco <rob.dimarco@416software.com>
2023-12-13 01:43:15 -0500

    Day 13 part 2

--- 2023/ruby/day13.rb
+++ 2023/ruby/day13.rb
@@ -18,14 +18,14 @@ TXT
 
 real = File.open("day13.txt").read.lines.map(&:strip)
 
-def match(pattern)
+def match(pattern, skip_score)
   i = 0
   loop do
     break if i >= pattern.size - 1
     above = pattern[0..i].reverse
     below = pattern[(i+1)..(2*i + 1)]
     above = above[0...below.size] 
-    return i if above == below
+    return i if above == below && skip_score != i
     i += 1
   end
   -1
@@ -60,24 +60,48 @@ def transform(pattern)
   rv
 end
 
-def part1(lines)
-  parse(lines).map do |p| 
-    i = match(p)
-    if i > -1
-      (i + 1) * 100
-    else
-      t = transform(p)
-      match(t) + 1
+def evaluate(p, skip_score = -2)
+  i = match(p, (skip_score/100 - 1))
+  if i > -1 && (i + 1) * 100 != skip_score
+    (i + 1) * 100
+  else
+    t = transform(p)
+    v = match(t, skip_score - 1) + 1
+    v == skip_score ? -2 : v
+  end
+end
+
+def with_increments(p)
+  orig = evaluate(p)
+  size = p.size * p[0].size
+  k = 0
+  loop do 
+    break if k >= size
+    np = p.map {|pp| pp.dup}
+    r = k / p[0].size
+    c = k % p[0].size
+    np[r][c] = np[r][c] == "#" ? '.' : '#'
+
+    v = evaluate(np, orig)
+    if v > 0
+      return v 
     end
-  end.sum
+    k += 1
+  end
+  -1
 end
 
-def part2(lines)
+def part1(lines)
+  parse(lines).map {|p| evaluate(p)}.sum
 end
 
-puts part1(sample).inspect
+def part2(lines)  
+  parse(lines).map {|p| with_increments(p)}.sum
+end
+
+puts part1(sample)
 puts part1(real)
 
-# puts part2(sample)
-# puts part2(real)
+puts part2(sample).inspect
+puts part2(real).inspect
 
