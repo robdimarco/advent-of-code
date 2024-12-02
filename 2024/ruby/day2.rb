25b8bc3635991629bfacafaf22a72b150b739025
Rob Di Marco <rob.dimarco@416software.com>
2024-12-02 00:36:30 -0500

    Day 2 part 2

--- 2024/ruby/day2.rb
+++ 2024/ruby/day2.rb
@@ -12,26 +12,45 @@ def parse_data(data)
   data.map(&:split).map {|n| n.map(&:to_i)}
 end
 
+def is_safe?(a)
+  vals = (0...(a.length - 1)).map do |k|
+    a[k] - a[k+1]
+  end
+  pos = vals.reject{|n| n == 0 }.count {|n| n > 0}
+  vals.map(&:abs).max < 4 && [0, vals.length].include?(pos) && !vals.include?(0)
+end
+
 def part1(data)
   d = parse_data(data)
   cnt = 0
-  d.each_with_index do |a, idx|
-    vals = (0...(a.length - 1)).map do |k|
-      a[k] - a[k+1]
-    end
-    pos = vals.reject{|n| n == 0 }.count {|n| n > 0}
-    if vals.map(&:abs).max < 4 && [0, vals.length].include?(pos) && !vals.include?(0)
-      cnt += 1
-    end
+  d.each do |a|
+    cnt += 1 if is_safe?(a)
   end
   cnt
 end
 
 def part2(data)
+  d = parse_data(data)
+  cnt = 0
+  d.each do |a|
+    if is_safe?(a)
+      cnt += 1 
+      next
+    end
+
+    (0...a.length).each do |idx|
+      b = a[0...idx] + a[idx+1...a.length]
+      if is_safe?(b)
+        cnt += 1
+        break
+      end
+    end
+  end
+  cnt
 end
 
 puts part1(TEST_DATA)
 puts part1(REAL_DATA) 
 
-# puts part2(TEST_DATA)
-# puts part2(REAL_DATA) 
+puts part2(TEST_DATA)
+puts part2(REAL_DATA) 
