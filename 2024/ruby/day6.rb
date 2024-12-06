e644e8df9fc880add72971732fcab7f9d3051592
Rob Di Marco <rob.dimarco@416software.com>
2024-12-06 08:33:29 -0500

    Day 6 part 2 faster!

--- 2024/ruby/day6.rb
+++ 2024/ruby/day6.rb
@@ -12,38 +12,6 @@ TEST_DATA =<<~DATA.lines.map(&:strip).map(&:chars)
 DATA
 REAL_DATA = File.read('day6.txt').lines.map(&:strip).map(&:chars)
 
-def build_map(data)
-  base = {
-    nodes: [],
-    lines: Hash.new(){|h,k| h[k] = []},
-  }
-  (0...data.size).each do |r|
-    (0...data[r].size).each do |c|
-      if data[r][c] == "^"
-        base[:start] = [r, c]
-      elsif data[r][c] == "#"
-        base[:nodes].push([r, c])
-      end
-    end
-  end
-
-  base[:nodes].each do |(r,c)|
-    [[0,1], [0,-1], [1,0],[1, -1]].each do |(dr, dc)|
-      paths = []
-      rr = r
-      cc = c
-      loop do
-        rr = rr + dr
-        cc = cc + dc
-        break if rr < 0 || cc < 0 || rr >= data.size || cc >= data[rr].size || data[rr][cc] == "#"
-        paths.push([rr, cc])
-      end
-      base[:lines][[r,c, dr, dc]].push([rr, cc]) if paths.any?
-    end
-  end
-  base
-end
-
 def move(data, pos, dir, path)
   row, col = pos
   dr, dc = dir
@@ -107,25 +75,84 @@ def part1(data)
   find_path(data).map {|h| h[:pos]}.uniq.count
 end
 
-def part2(data)
-  cnt = 0
-  start = find_start(data)
-  points = find_path(data).map {|h| h[:pos]}.uniq - [start]
-  puts "Possible #{points.size}"
-  points.each do |(r, c)|
-    data[r][c] = "#"
-    if find_loop(data)
-      cnt +=1 
-      print 'x'
-    else
-      print '.'
+def build_map(data)
+  base = {
+    collapsed: {
+      rows: Hash.new{|h, k| h[k] = []},
+      cols: Hash.new{|h, k| h[k] = []}
+    }
+  }
+  (0...data.size).each do |r|
+    (0...data[r].size).each do |c|
+      if data[r][c] == "^"
+        base[:start] = [r, c]
+      elsif data[r][c] == "#"
+        base[:collapsed][:rows][r].push(c)
+        base[:collapsed][:cols][c].push(r)
+      end
     end
-    data[r][c] = "."
+  end
+  # puts base.inspect
+  # base[:collapsed][:rows].values.map(&:sort!)
+  # base[:collapsed][:cols].values.map(&:sort!)
+  base
+end
+
+def has_loop(m)
+  rows = m[:collapsed][:rows]
+  cols = m[:collapsed][:cols]
+  r, c = m[:start]
+  dr, dc = [-1, 0]
+  path = [[r, c, dr, dc]]
+  loop do 
+    if dr == 1
+      nn = cols[c].select {|rr| rr > r}.min
+      break if nn.nil?
+      r = nn - 1
+      dr = 0
+      dc = -1
+    elsif dr == -1
+      nn = cols[c].select {|rr| rr < r}.max
+      break if nn.nil?
+      r = nn + 1
+      dr = 0
+      dc = 1
+    elsif dc == 1
+      nn = rows[r].select {|cc| cc > c}.min
+      break if nn.nil?
+      c = nn - 1
+      dr = 1
+      dc = 0
+    elsif dc == -1
+      nn = rows[r].select {|cc| cc < c}.max
+      break if nn.nil?
+      c = nn + 1
+      dr = -1
+      dc = 0
+    end
+    return true if path.include?([r, c, dr, dc])
+    path.push([r, c, dr, dc])
+  end
+
+  false
+end
+
+def part2(data)
+  m = build_map(data)
+  to_change = find_path(data).map {|h| h[:pos]}.uniq - [m[:start]]
+  cnt = 0
+  to_change.each do |(r, c)|
+    m[:collapsed][:rows][r].push(c)
+    m[:collapsed][:cols][c].push(r)
+    if has_loop(m)
+      cnt += 1 
+    end
+    m[:collapsed][:rows][r].reject!{|cc| cc == c}
+    m[:collapsed][:cols][c].reject!{|rr| rr == r}
   end
   cnt
 end
-
 # puts part1(TEST_DATA)
 # puts part1(REAL_DATA)
-# puts part2(TEST_DATA)
-# puts part2(REAL_DATA)
+puts part2(TEST_DATA)
+puts part2(REAL_DATA)
