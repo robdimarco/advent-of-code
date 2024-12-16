36382532a4733bca30e03508df150b7307d75ffa
Rob Di Marco <rob.dimarco@416software.com>
2024-12-16 01:21:24 -0500

    Day 16 part 2

--- 2024/ruby/day16.rb
+++ 2024/ruby/day16.rb
@@ -57,5 +57,39 @@ def part1(data)
   end
   rv
 end
-puts part1(TEST_DATA)
-puts part1(REAL_DATA)
+# puts part1(TEST_DATA)
+# puts part1(REAL_DATA)
+
+
+def part2(data)
+  course, start, finish = parse(data)
+
+  visited = {}
+  to_check = [[start, 0 + 1i, 0, [start]]]
+  rv = nil
+  finish_paths = []
+  while to_check.any?
+    pos, dir, cost, path = to_check.shift
+
+    next if course[pos] == "#" # At wall... not valid
+    if !visited.key?([pos, dir]) || visited[[pos, dir]] >= cost
+      visited[[pos, dir]] = cost
+      if pos != finish
+        to_check.push([pos + dir, dir, cost + 1, path + [pos + dir]])
+        to_check.push([pos, dir * 1i, cost + 1_000, path])
+        to_check.push([pos, dir * -1i, cost + 1_000, path])
+      else 
+        if rv.nil? || rv > cost
+          rv = cost
+          finish_paths = [path]
+        elsif rv == cost
+          finish_paths.push(path)
+        end
+      end
+    end
+  end
+  # puts finish_paths.flatten.uniq.inspect
+  finish_paths.flatten.uniq.size
+end
+puts part2(TEST_DATA)
+puts part2(REAL_DATA)
