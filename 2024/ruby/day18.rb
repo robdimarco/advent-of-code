c700c324dca31d9c2a3de61f10d32b047d1ea4ee
Rob Di Marco <robdimarco@render.com>
2024-12-18 00:42:53 -0500

    Day 18 part 2

--- 2024/ruby/day18.rb
+++ 2024/ruby/day18.rb
@@ -68,10 +68,57 @@ def min_path(drops, target)
   solutions.map(&:size).min
 end
 
+def has_path(drops, target)
+  pos = 0 + 0i
+  to_check = Containers::PriorityQueue.new
+  DIRS.each do |dir|
+    to_check.push([pos, dir, []], [0, -target.abs])
+  end
+
+  visited = {Complex(0,0) => 0}
+  until to_check.empty? do
+    pos, dir, path = to_check.pop
+    new_pos = pos + dir
+    new_path = path + [new_pos]
+    next if new_pos.real < 0 || new_pos.imag < 0 || new_pos.real > target.real || new_pos.imag > target.imag
+    next if visited[new_pos] && new_path.size >= visited[new_pos].to_i
+
+    # puts "#{new_path.size} #{new_pos}"
+    visited[new_pos] = new_path.size
+    if new_pos == target
+      return new_path
+    elsif drops.include?(new_pos)
+      next
+    else
+      DIRS.each do |new_dir|
+        to_check.push([new_pos, new_dir, new_path], [new_path.size, -1 * (new_pos - target).abs])
+      end
+    end
+  end
+
+  nil
+end
+
 def part1(data, time, target)
   drops = parse(data)
   min_path(drops[0...time], target)
 end
 
-puts part1(TEST_DATA, 12, 6+6i)
-puts part1(REAL_DATA, 1024, 70+70i)
+def part2(data, target)
+  drops = parse(data)
+  i = drops.size - 1
+  current_path = nil
+  loop do 
+    raise "doh" if i < 0
+    current_path = has_path(drops[0..i], target)
+
+    return drops[i+1] unless current_path.nil?
+
+    i -= 1
+  end
+end
+
+# puts part1(TEST_DATA, 12, 6+6i)
+# puts part1(REAL_DATA, 1024, 70+70i)
+puts part2(TEST_DATA, 6+6i)
+puts part2(REAL_DATA, 70+70i)
