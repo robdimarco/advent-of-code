2f9deca2990e97005d0aa4761a2b2043bfd9f02c
Rob Di Marco <rob.dimarco@416software.com>
2024-12-20 13:33:43 -0500

    Day 20 part 2

--- 2024/ruby/day20.rb
+++ 2024/ruby/day20.rb
@@ -33,6 +33,7 @@ def parse(data)
   end
   [course, start, finish, data.lines.size, data.lines[0].size]
 end
+
 require 'algorithms'
 DIRS = [1, -1, 1i, -1i]
 def md(a,b)
@@ -66,8 +67,6 @@ def part1(data, min_save)
   rv = Hash.new(0)
   course.keys.each do |wall|
     DIRS.each do |k|
-      require 'debug'
-      # binding.break if wall == 7+6i
       a = wall - k
       b = wall + k
       if dist_from_start.key?(a) && dist_from_finish.key?(b)
@@ -78,5 +77,43 @@ def part1(data, min_save)
   end
   rv.keys.map {|k| k >= min_save ? rv[k] : 0}.sum
 end
-puts part1(TEST_DATA, 0)
-puts part1(REAL_DATA, 100)
+
+def part2(data, min_save)
+  max_cheat_path = 20
+  course, start, finish, max_r, max_i = parse(data)
+  dist_from_finish = find_dist(course, start, finish, max_r, max_i)
+  dist_from_start = find_dist(course, finish, start, max_r, max_i)
+  reg_leng = dist_from_finish[start]
+
+  visited = {}
+  dist_from_start.keys.each do |spos|
+    s_dist = dist_from_start[spos]
+    (0..max_cheat_path).each do |r_steps|
+      (0..(max_cheat_path - r_steps)).each do |c_steps|
+        next if r_steps + c_steps > max_cheat_path
+        [1, -1].each do |r_dir|
+          [1i, -1i].each do |c_dir|
+            n_pos = spos + r_steps * r_dir + c_steps * c_dir
+            f_dist = dist_from_finish[n_pos]
+            next if f_dist.nil?
+            savings = reg_leng - (s_dist + f_dist + c_steps + r_steps)
+
+            if savings >= min_save
+              key = [spos, n_pos]
+              if visited[key].nil? || visited[key] < savings
+                visited[key] = savings
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  cnts = Hash.new(0)
+  visited.values.each {|v| cnts[v] += 1}
+  cnts.values.sum
+end
+# puts part1(TEST_DATA, 0)
+# puts part1(REAL_DATA, 100)
+puts part2(TEST_DATA, 50)
+puts part2(REAL_DATA, 100)
