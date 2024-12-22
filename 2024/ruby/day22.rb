fc0e351392ea174343b9cf15554ed35c1e044424
Rob Di Marco <rob.dimarco@416software.com>
2024-12-22 01:08:48 -0500

    Day 22 part 2

--- 2024/ruby/day22.rb
+++ 2024/ruby/day22.rb
@@ -30,5 +30,56 @@ def part1(data, iters)
     n
   end.sum
 end
-puts part1(TEST_DATA, 2000)
-puts part1(REAL_DATA, 2000)
+
+def secrets_from_line(line, iters)
+  n = line.to_i
+  secrets = [n]
+  secrets += (0...iters).map do
+    n = evolve(n)
+  end
+end
+
+def diffs_from_secrets(secrets)
+  (1...secrets.size).map do |idx| 
+    (secrets[idx] % 10) - (secrets[idx - 1] % 10)
+  end
+end
+
+def bananas_by_sequence(line, iters)
+  hsh = Hash.new {|h,k| h[k] = []}
+  secrets = secrets_from_line(line, iters)
+  diffs = diffs_from_secrets(secrets)
+  (0...diffs.size - 4).each do |idx|
+    seq = diffs[idx..idx+3]
+    val = secrets[idx+4] % 10
+    hsh[seq].push(val)
+  end
+  hsh
+end
+
+require 'debug'
+def part2(data, iters)
+  mappings = []
+  data.lines.map do |line|
+    mappings.push(bananas_by_sequence(line, iters))
+  end
+
+  summer = Hash.new(0)
+  mappings.each do |hsh|
+    hsh.each do |k, v|
+      summer[k] += v[0]
+    end
+  end
+  # binding.break
+  summer.values.sort.reverse[0]
+end
+# puts part1(TEST_DATA, 2000)
+# puts part1(TEST_DATA, 2000)
+TEST_DATA_2 = <<~DATA
+1
+2
+3
+2024
+DATA
+puts part2(TEST_DATA_2, 2000)
+puts part2(REAL_DATA, 2000)
