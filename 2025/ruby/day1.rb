eff5a892056e96dfb5eeb14ac26a8652ce33f26b
Rob Di Marco <rob.dimarco@416software.com>
2025-12-01 00:19:35 -0500

    Day 1 2025

---
+++ 2025/ruby/day1.rb
@@ -0,0 +1,57 @@
+TEST_DATA =<<~DATA.lines.map(&:strip)
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+DATA
+REAL_DATA = File.read('day1.txt').lines.map(&:strip)
+
+
+def part1(data)
+  pos = 50
+  cnt = 0
+  data.each do |line|
+    dir, *amt = line.chars
+    amt = amt.join.to_i
+    amt *= -1 if dir == 'L'
+    pos = (pos + amt) % 100
+    cnt += 1 if pos == 0
+  end
+  cnt
+end
+
+def part2(data)
+  pos = 50
+  cnt = 0
+  data.each do |line|
+    dir, *amt = line.chars
+    amt = amt.join.to_i
+    cnt += amt / 100
+    # puts ("Adding #{amt / 100} for line #{line}")
+    amt = amt % 100
+    amt *= -1 if dir == 'L'
+
+    pos_n = pos + amt
+    if pos != 0 && (pos_n <= 0 || pos_n >= 100)
+      cnt += 1 
+      # puts "Adding 1 to count for line #{line}"
+    end
+    pos = pos_n % 100
+    # cnt +=1 if pos == 0
+  end
+  cnt
+end
+
+# puts part1(TEST_DATA)
+# puts part1(REAL_DATA) 
+
+puts part2(TEST_DATA)
+puts part2(REAL_DATA) 
+# puts part2(["R1000"])
+# puts part2(["L1000"])
