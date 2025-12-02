e3b913b05899b45b818f9d9eafe8ec6bb9bf2a34
Rob Di Marco <rob.dimarco@416software.com>
2025-12-02 00:10:24 -0500

    Day 2 part 2

--- 2025/ruby/day2.rb
+++ 2025/ruby/day2.rb
@@ -25,12 +25,29 @@ def part1(data)
 end
 
 def part2(data)
+  cnt = 0
+  data.split(',').each do |range|
+    range_start, range_end = range.split('-').map(&:to_i)
+    (range_start..range_end).each do |num|
+      v = num.to_s
+      vals = []
+      (1..v.length / 2).each do |size|
+        s = v[0, size]
+        re = Regexp.new("^(#{s})+$")
+        if v =~ re && !vals.include?(num)
+          vals.push(num)
+          cnt += num
+        end
+      end
+    end
+  end
+  cnt
 end
 
 puts "Part 1"
 puts part1(TEST_DATA)
 puts part1(REAL_DATA) 
 
-# puts "Part 2"
-# puts part2(TEST_DATA)
-# puts part2(REAL_DATA) 
+puts "Part 2"
+puts part2(TEST_DATA)
+puts part2(REAL_DATA) 
