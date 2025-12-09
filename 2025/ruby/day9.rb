eb0418a7e7720b0e2ba9c595cd761415e295d659
Rob Di Marco <rob.dimarco@416software.com>
2025-12-09 01:49:49 -0500

    Day 9 part 2

--- 2025/ruby/day9.rb
+++ 2025/ruby/day9.rb
@@ -31,9 +31,37 @@ def part1(data)
 end
 
 def part2(data)
+  points = parse(data)
+  lines = []
+  points.each_with_index do |p1, i|
+    if i < points.length - 1
+      p2 = points[i+1]
+      lines << [p1, p2]
+    else
+      lines << [p1, points[0]]
+    end
+  end
+
+  areas = []
+  points.each_with_index do |p1, i|
+    points[i+1..-1].each_with_index do |p2, j|
+      intersect = lines.any? do |line|
+        lx1, lx2 = [line[0][0], line[1][0]].sort
+        ly1, ly2 = [line[0][1], line[1][1]].sort
+
+        x1, x2 = [p1[0], p2[0]].sort
+        y1, y2 = [p1[1], p2[1]].sort
+
+        lx1 < x2 && lx2 > x1 && ly1 < y2 && ly2 > y1
+
+      end
+      areas << area(p1, p2) unless intersect
+    end
+  end
+  areas.max
 end
 
-puts "Part 1"
+# puts "Part 1"
 puts part1(TEST_DATA)
 puts part1(REAL_DATA) 
 
