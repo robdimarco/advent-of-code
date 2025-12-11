30e1a613e58aa17c53789658fe15a092831544f9
Rob Di Marco <rob.dimarco@416software.com>
2025-12-11 01:04:57 -0500

    Day 11 part 2

--- 2025/ruby/day11.rb
+++ 2025/ruby/day11.rb
@@ -42,7 +42,45 @@ def part1(data)
   cnt
 end
 
+TEST_DATA_2 = <<~DATA
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+DATA
+
+def path_count(connections, counts, src, target)
+  return 1 if src == target
+  return 0 unless connections[src]
+  k = [src, target]
+  return counts[k] if counts[k]
+  cnt = 0
+  connections[src].each do |n|
+    cnt += path_count(connections, counts, n, target)
+  end
+  counts[k] = cnt
+end
+
 def part2(data)
+  counts = {}
+  connections = parse(data)
+  a1 = path_count(connections, counts, "svr", "fft")
+  b1 = path_count(connections, counts, "fft", "dac")
+  c1 = path_count(connections, counts, "dac", "out")
+
+  a2 = path_count(connections, counts,"svr", "dac")
+  b2 = path_count(connections, counts,"dac", "fft")
+  c2 = path_count(connections, counts,"fft", "out")
+  (a1 * b1 * c1) + (a2 * b2 * c2)
 end
 
 puts "Part 1"
@@ -50,5 +88,5 @@ puts part1(TEST_DATA)
 puts part1(REAL_DATA) 
 
 puts "Part 2"
-puts part2(TEST_DATA)
+puts part2(TEST_DATA_2)
 puts part2(REAL_DATA) 
